a559bcece2ecb5ede011c356534ad6b8d800322d
Dan Rice <dan.rice.92@outlook.com>
2016-08-31 12:43:32 -0600

    Blocmetrics 6: API Controller completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :registeredapps
 
   devise_for :users
+
+  namespace :api, defaults: { format: :json } do
+  	match '/events', to: 'events#preflight', via: [:options]
+  	resources :events, only: [:create]
+  end
   
   get 'about' => 'home#about'
 
